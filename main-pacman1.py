436906e1b26d3bcd43b4dfce9f6e639573109a4a
andressaa28 <andressaaalbano@gmail.com>
2025-04-30 08:48:30 -0300

    ultima versão do main

--- main-pacman1.py
+++ main-pacman1.py
@@ -7,26 +7,31 @@ pygame.init()
 
 # tamanho da tela
 TILE_SIZE = 32
-SCREAN_WIDTH = 448
-SCREAN_HEIGHT = 496
+SCREEN_WIDTH = 448
+SCREEN_HEIGHT = 496
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pac-Man Simples")
 
-#configurar a janela
-tela = pygame.display.set_mode ((448, 496))
-pygame.display.set_caption('fantasma verde')
+# tela inicial
+def tela_inicial():
+     fonte_titulo = pygame.font.pygame.font.SysFont("Arial", 48, bold=False, italic=False)
 
-# função para carregar imagem
+#imagem do mapa
+
+# imagem do pac-man
+
+# função para carregar imagem de fantasma
 def carregar_imagem():
-      imagens = []
+    imagens = []
     for i in range(1, 4):  # 1 até 3
         caminho = os.path.join('imagens', f'fantasma{i}.png')
         imagem = pygame.image.load(caminho).convert_alpha()
         imagens.append(imagem)
     return imagens
 
-# escolher a imagem que quer usar
-fantasma_verde = carregar_imagem('Fantasma verde.png')
+# carregar imagem do fantasma verde
+fantasma_img = pygame.image.load("fantasma_verde.png")
+fantasma_img = pygame.transform.scale(fantasma_img, (32, 32))
 
 # cores
 class colors:
@@ -42,16 +47,22 @@ class colors:
         AQUA_GREEN = (52, 203, 120) # verde água                                         
         DARK_YELLOW = (136, 158, 28) # amarelo escuro 
         LIGHT_YELLOW = (163, 188, 40) # ammarelo claro
+        BLACK = (0, 0, 0) # preto
+        WHITE = (253, 252, 252) # branco
+        LIGHT_BLUE = (172, 188, 233) # azul escuro
+        DARK_BLUE = (7, 57, 167) # azul escuro
+        LIGHTER_BLUE = (203, 231, 246) # azul mais claro
+        RED = (244, 12, 12) # vermelho
 
 # função para carregar um nível de um arquivo .txt
 def load_level(level_num):
-     try:
-          with open(f"levels/level{level_num}.txt") as f:
-               map_data = [line.strip() for line in f.readlines()]
-          return map_data
-     except FileNotFoundError:
-          print(f"Erro: levels/level{level_num}.txt não encontrado.")
-          sys.exit()
+    try:
+        with open(f"levels/level{level_num}.txt") as f:
+            map_data = [line.strip() for line in f.readlines()]
+        return map_data
+    except FileNotFoundError:
+        print(f"Erro: levels/level{level_num}.txt não encontrado.")
+        sys.exit()
 
 # exemplo de nível hardcoded (como dicionário python)
 level1 = {
@@ -61,7 +72,7 @@ level1 = {
         "1011111101",
         "1000000001",
         "1111111111"
-     ]
+     ],
      "pacman_start": [1, 1],
      "ghost_den": [],
      "extra_voids": [],
@@ -81,24 +92,20 @@ pacman_x = level1["pacman_start"][0] * TILE_SIZE
 pacman_y = level1["pacman_start"][1] * TILE_SIZE
 direction = (0, 0)
 
-# FPS (quadros por segundo) 
 # ex: FPS = 60 -> o jogo tenta rodar as 60 atualizações por segundo, o que é considerado suave e padrão em jogos
+# FPS (quadros por segundo) 
 clock = pygame.time.Clock()
 FPS = 60
 
 # loop principl do jogo 
 running = True
 while running:
-     screen.fill(Colors.LIGHT_GREY)
+     screen.fill(colors.LIGHT_GREY)
 
      # eventos
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
                running = False
-
-     # desenhar o mapa
-     
-     # desenhar pac-man
 
      # atualiza a tela
      pygame.pygame.flip()
